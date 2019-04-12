6ecb03243729053faab78e9fb7cf94864c25d354
Dmitry <flatorez@ya.ru>
2019-04-12 21:10:31 +0200

    add a logic to show the fake decks

---
+++ src/components/deck-list/deck-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './deck-list.css';
+
+const DeckList = () => {
+  return (
+    <ul>
+      <li>English Common Idioms</li>
+    </ul>
+  );
+};
+
+export default DeckList;
---
+++ src/components/deck-list/index.js
@@ -0,0 +1,3 @@
+import DeckList from './deck-list';
+
+export default DeckList;
