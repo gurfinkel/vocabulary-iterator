9f9be003da9b5a3a4c7ce5754ea76724bb8c469e
Dmitry <flatorez@ya.ru>
2019-04-17 07:08:42 +0200

    add provider and consumer for the vocabulary-api-service

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,25 +5,40 @@ import CardDetails from '../card-details';
 import DeckDetails from '../deck-details';
 import DeckList from '../deck-list';
 import Header from '../header';
+import VocabularyApiService from '../../services/vocabulary-api-service';
 
 import './app.css';
 
+const service = new VocabularyApiService();
+const { Provider, Consumer } = React.createContext();
+const DeckListWithService = () => {
+  return (
+    <Consumer>
+      {
+        ({getDecks}) => <DeckList getData={getDecks} />
+      }
+    </Consumer>
+  )
+};
+
 const App = () => {
   return (
-    <Router>
-      <div className="vocabulary-iterator-app">
-        <Header/>
-        <Route path='/decks' exact component={DeckList} />
-        <Route path='/decks/:id' render={({match}) => {
-          const {id} = match.params;
-          return <DeckDetails itemId={id} />
-        }}/>
-        <Route path='/cards/:id' render={({match}) => {
-          const {id} = match.params;
-          return <CardDetails itemId={id} />
-        }} />
-      </div>
-    </Router>
+    <Provider value={service}>
+      <Router>
+        <div className="vocabulary-iterator-app">
+          <Header/>
+          <Route path='/decks' exact component={DeckListWithService} />
+          <Route path='/decks/:id' render={({match}) => {
+            const {id} = match.params;
+            return <DeckDetails itemId={id} />
+          }}/>
+          <Route path='/cards/:id' render={({match}) => {
+            const {id} = match.params;
+            return <CardDetails itemId={id} />
+          }} />
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
--- src/components/deck-list/deck-list.js
+++ src/components/deck-list/deck-list.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
 
-import VocabularyApiService from '../../services/vocabulary-api-service';
-
 import './deck-list.css';
 
-const service = new VocabularyApiService();
-
-const DeckList = ({history}) => {
-  const decks = service.getDecks().map(({id, name, dueCards, newCards}) => {
+const DeckList = ({getData, history}) => {
+  const decks = getData().map(({id, name, dueCards, newCards}) => {
     return (
       <tr key={id} className="table-light" onClick={() => history.push(`${id}`)}>
         <th scope="row">{name}</th>
